c87b74b6dde50ae0b3e26be7191e273f7151898e
Evgeniy <zheinirk38@gmail.com>
2022-12-11 23:46:53 +0800

    Добавил метод take в класс Player

--- functions.py
+++ functions.py
@@ -8,6 +8,6 @@ def draw():
     else:
         return 1
 
-# def start_game(p1, p2, start, candies):
-#     if start == 0:
-#         p1.
+def start_game(p1, p2, start, candies):
+    if start == 0:
+        p1.
--- player.py
+++ player.py
@@ -2,3 +2,6 @@ class Player:
     def __init__(self, name) -> None:
         self.name = name
         print(f"{name} вошёл в игру!")
+
+    def take(cand, n):
+        return cand -n
