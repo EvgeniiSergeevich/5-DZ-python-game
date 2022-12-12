29609be44d7a0c078be6e923a5770bac756624b4
Evgeniy <zheinirk38@gmail.com>
2022-12-12 19:14:00 +0800

    Добавил исключения в game. Если при выборе количества игроков указать не 1 или 2, вылетит сообщение, указывающее на ошибку

--- game.py
+++ game.py
@@ -3,43 +3,46 @@ import functions
 
 spl = '__________________________________________\n'                    # Просто разделитель
 print(spl)
+try:
+    pl2_or_bot = int(input('Введите количество игроков: '))                 # Выбор 1 или 2х игроков
 
-pl2_or_bot = int(input('Введите количество игроков: '))                 # Выбор 1 или 2х игроков
+    players = []                                                            # Список с игроками, ддля удобства
 
-players = []                                                            # Список с игроками, ддля удобства
+    if pl2_or_bot == 2:                   
+        print(spl)                                                          # Создаю 2х игроков. Изначально был класс Bot, 
+        name_p1 = input('Введите имя первого игрока: ')                     # но решил обойтись без него.
+        player1 = player.Player(name_p1)
+        print(spl)
+        name_p2 = input('Введите имя второго игрока: ')
+        player2 = player.Player(name_p2)
+        players = [player1, player2]
+    elif pl2_or_bot == 1:
+        print(spl)
+        name_p1 = input('Введите Ваше имя: ')
+        print(spl)
+        player1 = player.Player(name_p1)
+        bot1 = player.Player('ИИ Джарвис')
+        print(spl)
+        players = [player1, bot1]
+    else:
+        print('Введите 1 или 2!!')
+        
+    start = functions.draw()                                                # Жеребъёвка. Получаю 1 или 0. 0 - первый игрок, 1 - второй
 
-if pl2_or_bot == 2:                   
-    print(spl)                                                          # Создаю 2х игроков. Изначально был класс Bot, 
-    name_p1 = input('Введите имя первого игрока: ')                     # но решил обойтись без него.
-    player1 = player.Player(name_p1)
     print(spl)
-    name_p2 = input('Введите имя второго игрока: ')
-    player2 = player.Player(name_p2)
-    players = [player1, player2]
-elif pl2_or_bot == 1:
+    print('Жеребъёвка...')
     print(spl)
-    name_p1 = input('Введите Ваше имя: ')
+    print(f'Начинает игрок №{start + 1} - {players[start].name}')
     print(spl)
-    player1 = player.Player(name_p1)
-    bot1 = player.Player('ИИ Джарвис')
-    print(spl)
-    players = [player1, bot1]
-else:
-    print('Введите 1 или 2!!')
-    
 
+    candies = 117
 
-start = functions.draw()                                                # Жеребъёвка. Получаю 1 или 0. 0 - первый игрок, 1 - второй
-
-print(spl)
-print('Жеребъёвка...')
-print(spl)
-print(f'Начинает игрок №{start + 1} - {players[start].name}')
-print(spl)
-
-candies = 117
-
-if pl2_or_bot == 2:
-    functions.start_game(players[0].name, players[1].name, start, candies) 
-else:
-    functions.start_game_bot(players[0].name, players[1].name, start, candies)
+    if pl2_or_bot == 2:
+        functions.start_game(players[0].name, players[1].name, start, candies) 
+    else:
+        functions.start_game_bot(players[0].name, players[1].name, start, candies)
+except ValueError:
+    print("Вы ввели не число!")
+except:
+    print('Число игроков должно быть 1 или 2!')    
+        
